7050221a63d4753943639c83460e8593c4ca4421
Nathan <nathan.llucasl@gmail.com>
2025-01-23 08:56:04 -0300

    POST event

--- src/main/java/com/devsuperior/demo/controllers/EventController.java
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/events")
@@ -21,5 +22,12 @@ public class EventController {
     public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
         Page<EventDTO> result = eventService.findAllPageable(pageable);
         return ResponseEntity.ok(result);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventDTO> save(@RequestBody EventDTO dto) {
+        EventDTO result = eventService.insertEvent(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(result.getId()).toUri();
+        return ResponseEntity.created(uri).body(result);
     }
 }
--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -1,6 +1,9 @@
 package com.devsuperior.demo.services;
 
 import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.entities.Event;
+import com.devsuperior.demo.repositories.CityRepository;
 import com.devsuperior.demo.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,10 +16,30 @@ public class EventService {
 
     @Autowired
     private EventRepository eventRepository;
+    @Autowired
+    private CityRepository cityRepository;
 
     @Transactional(readOnly = true)
     public Page<EventDTO> findAllPageable(Pageable pageable) {
         return eventRepository.findAll(pageable)
                 .map(EventDTO::new);
+    }
+
+    @Transactional
+    public EventDTO insertEvent(EventDTO dto) {
+        Event event = copyDtoToEntity(dto);
+        Event result = eventRepository.save(event);
+        return new EventDTO(result);
+    }
+
+    private Event copyDtoToEntity(EventDTO dto) {
+        Event event = new Event();
+        event.setName(dto.getName());
+        event.setDate(dto.getDate());
+        event.setUrl(dto.getUrl());
+        City eventCity = cityRepository.findById(dto.getCityId()).get();
+        event.setCity(eventCity);
+        return event;
+
     }
 }
