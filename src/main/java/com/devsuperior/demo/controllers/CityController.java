6feb95f027cc4412d994ab0165fbb89620c985e9
Nathan <nathan.llucasl@gmail.com>
2025-01-23 08:47:09 -0300

    add validation

--- src/main/java/com/devsuperior/demo/controllers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -2,6 +2,7 @@ package com.devsuperior.demo.controllers;
 
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.services.CityService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +25,7 @@ public class CityController {
     }
 
     @PostMapping
-    public ResponseEntity<CityDTO> createCity(@RequestBody CityDTO dto) {
+    public ResponseEntity<CityDTO> createCity(@Valid @RequestBody CityDTO dto) {
         CityDTO result = cityService.insertCity(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(result.getId()).toUri();
         return ResponseEntity.created(uri).body(result);
--- src/main/java/com/devsuperior/demo/dto/CityDTO.java
+++ src/main/java/com/devsuperior/demo/dto/CityDTO.java
@@ -1,10 +1,12 @@
 package com.devsuperior.demo.dto;
 
 import com.devsuperior.demo.entities.City;
+import jakarta.validation.constraints.NotBlank;
 
 public class CityDTO {
 	
 	private Long id;
+	@NotBlank
 	private String name;
 	
 	public CityDTO() {
