9a9a69216a23586c2ff80ed7c67c32e6facdc727
Nathan <nathan.llucasl@gmail.com>
2025-01-23 08:22:20 -0300

    add: basic layers

---
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -0,0 +1,14 @@
+package com.devsuperior.demo.controllers;
+
+import com.devsuperior.demo.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+}
